410f9334519b43415b39c4c6bbb1b41a99db355a
pr7h <prthpatel412@gmail.com>
2020-04-22 00:32:56 -0400

    love and home ejs

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Git 2.0");
+var express = require("express");
+var app = express();
+
+app.get('/', (req,res)=>{
+  res.render("home.ejs");
+});
+
+app.get('/fallinlove/:thing', (req,res)=>{
+  var thing = req.params.thing;
+  //res.send("you fell in love with "+ thing);
+  res.render("love.ejs", {thingVAR: thing});  // thingVAR is being filled by thing in love.ejs
+})
+app.listen(3000, () => {
+  console.log("server connected!");
+});
